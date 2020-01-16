740c7695cd5a6a4e3537e9f5eda624900af2a14b
MnunesA <matheusna100@gmail.com>
2020-01-15 23:23:03 -0300

    Adicionada a pasta 'driver' dentro de commands

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.SubsystemDriver;
+import frc.robot.commands.driver.CommandDriver;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -19,8 +20,8 @@ import frc.robot.subsystems.SubsystemDriver;
  * project.
  */
 public class Robot extends TimedRobot {
+  
   private Command m_autonomousCommand;
-
   private RobotContainer m_robotContainer;
   private SubsystemDriver subsystemDriver;
   private CommandDriver commandDriver;
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,7 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.commands.CommandDriver;
+import frc.robot.commands.driver.CommandDriver;
 import frc.robot.subsystems.SubsystemDriver;
 
 public class RobotContainer {
